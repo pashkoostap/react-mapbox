f2735521b1b9d1c2827208bf589f570cb2760ba6
Pashko Ostap <pashko.ostap@gmail.com>
2019-07-28 23:32:44 +0300

    updated App module with remove handlers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
 import Map from './Map';
+import { timingSafeEqual } from 'crypto';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      markers: [
-        {
-          coordinates: [24, 48],
-          score: 0
-        },
-        {
-          coordinates: [24.05, 48.05],
-          score: 55
-        }
-      ]
+      selected: null,
+      markers: []
     };
 
     this.onMarkerMove = this.onMarkerMove.bind(this);
     this.onMarkerAdd = this.onMarkerAdd.bind(this);
+    this.togglePopup = this.togglePopup.bind(this);
+    this.onScoreChange = this.onScoreChange.bind(this);
+    this.onMarkerRemove = this.onMarkerRemove.bind(this);
   }
 
   onMarkerMove(marker, index) {
@@ -32,22 +28,60 @@ class App extends Component {
     });
   }
 
+  onScoreChange(add = true) {
+    this.setState(prev => {
+      const markers = prev.markers.map((item, i) => {
+        if (prev.selected === i) {
+          item.score = add ? item.score + 1 : item.score - 1;
+        }
+
+        return item;
+      });
+
+      return { ...prev, markers };
+    });
+  }
+
   onMarkerAdd(coordinates) {
     this.setState(prev => ({
       ...prev,
+      selected: null,
       markers: [...prev.markers, { coordinates, score: 0 }]
     }));
   }
 
+  onMarkerRemove() {
+    this.setState(prev => ({
+      ...prev,
+      selected: null,
+      markers: prev.markers.filter((item, i) => i !== prev.selected)
+    }));
+  }
+
+  togglePopup(marker) {
+    this.setState({ selected: marker });
+  }
+
   render() {
-    const { markers } = this.state;
+    const { markers, selected } = this.state;
 
     return (
-      <Map
-        markers={markers}
-        onMarkerMove={this.onMarkerMove}
-        onMarkerAdd={this.onMarkerAdd}
-      />
+      <div>
+        {markers.map((item, i) => (
+          <div key={i}>
+            {i}: {item.score}
+          </div>
+        ))}
+        <Map
+          markers={markers}
+          selected={selected}
+          onMarkerMove={this.onMarkerMove}
+          onMarkerAdd={this.onMarkerAdd}
+          togglePopup={this.togglePopup}
+          onScoreChange={this.onScoreChange}
+          onMarkerRemove={this.onMarkerRemove}
+        />
+      </div>
     );
   }
 }
--- src/Map.js
+++ src/Map.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+import ReactMapboxGl, { Layer, Feature, Popup } from 'react-mapbox-gl';
 
 const Map = ReactMapboxGl({
   accessToken: process.env.TOKEN
@@ -10,54 +10,98 @@ const updateMarkerValues = (marker, lngLat) => ({
   score: marker.score
 });
 
-const MapWrapper = ({ markers, onMarkerAdd, onMarkerMove }) => (
-  <Map
-    style='mapbox://styles/mapbox/streets-v9'
-    onClick={(map, e) => onMarkerAdd([e.lngLat.lng, e.lngLat.lat])}
-    containerStyle={{
-      height: '100vh',
-      width: '100vw'
-    }}
-    center={[24, 48]}
-    zoom={[10]}
-  >
-    <Layer
-      type='circle'
-      id='marker'
-      paint={{
-        'circle-color': 'red',
-        'circle-stroke-width': 2,
-        'circle-stroke-color': '#fff',
-        'circle-stroke-opacity': 1,
-        'circle-color': [
-          'match',
-          ['get', 'score'],
-          'Low',
-          '#fbb03b',
-          'Black',
-          '#223b53',
-          'Hispanic',
-          '#e55e5e',
-          'Asian',
-          '#3bb2d0',
-          /* other */ '#ccc'
-        ]
+const MapWrapper = ({
+  markers,
+  onMarkerAdd,
+  onMarkerMove,
+  selected,
+  togglePopup,
+  onScoreChange,
+  onMarkerRemove
+}) => {
+  const selectedMarker = markers.find((item, i) => i === selected);
+
+  return (
+    <Map
+      style='mapbox://styles/mapbox/streets-v9'
+      onClick={(map, e) => {
+        onMarkerAdd([e.lngLat.lng, e.lngLat.lat]);
       }}
+      containerStyle={{
+        height: '100vh',
+        width: '100vw'
+      }}
+      center={[24, 48]}
+      zoom={[10]}
     >
-      {markers.map((item, i) => (
-        <Feature
-          coordinates={item.coordinates}
-          properties={{ 'score': 'Low' }}
-          draggable={true}
-          key={i}
-          onDragEnd={e => {
-            onMarkerMove(updateMarkerValues(item, e.lngLat), i);
-          }}
-        />
-      ))}
-    </Layer>
-    )}
-  </Map>
-);
+      <Layer
+        type='circle'
+        id='marker'
+        paint={{
+          'circle-color': 'red',
+          'circle-stroke-width': 2,
+          'circle-stroke-color': '#fff',
+          'circle-stroke-opacity': 1,
+          'circle-radius': {
+            'base': 15,
+            'stops': [[10, 16], [22, 40]]
+          },
+          'circle-color': [
+            'match',
+            ['get', 'score'],
+            'Low',
+            '#fbb03b',
+            'Black',
+            '#223b53',
+            'Hispanic',
+            '#e55e5e',
+            'Asian',
+            '#3bb2d0',
+            /* other */ '#ccc'
+          ]
+        }}
+      >
+        {markers.map((item, i) => (
+          <Feature
+            coordinates={item.coordinates}
+            properties={{ 'score': 'Low' }}
+            draggable={true}
+            onMouseEnter={e => togglePopup(i)}
+            key={i}
+            onDragEnd={e => {
+              onMarkerMove(updateMarkerValues(item, e.lngLat), i);
+            }}
+          />
+        ))}
+      </Layer>
+      {selectedMarker && (
+        <Popup coordinates={selectedMarker.coordinates}>
+          <div>
+            <div>
+              Score: {selectedMarker.score}
+              <button
+                onClick={() => {
+                  onScoreChange();
+                }}
+              >
+                +
+              </button>
+              <button
+                onClick={() => {
+                  onScoreChange(false);
+                }}
+              >
+                -
+              </button>
+            </div>
+
+            <button onClick={onMarkerRemove}>Remove marker</button>
+          </div>
+        </Popup>
+      )}
+      )}
+    </Map>
+  );
+};
 
 export default MapWrapper;
